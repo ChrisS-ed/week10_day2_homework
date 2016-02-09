39c6edbbef443f4f326e3aa0c659190da0802cbb
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-02-09 17:23:21 +0000

    created views folder and files

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
-
+var expressLayouts = require('express-ejs-layouts');
 var http = require('http');
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
 app.get('/weather', function(req, res) {
-  res.send('Hello World!') 
+  res.render('weather') 
 });
 
 app.get('/weather/:location', function(request, response) {
